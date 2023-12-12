cc524b93fa166f85a51b0246f92fa1306ce7b7e7
Nguyen Van Tuong Nhat <tuongnhatnv@gmail.com>
2023-12-12 12:11:46 +0700

    up the first time

---
+++ crud_homework/database/database.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Connect to database
+ */
+function db() {
+
+}
+
+/**
+ * Create new student record
+ */
+function createStudent($value) {
+
+}
+
+/**
+ * Get all data from table student
+ */
+function selectAllStudents() {
+
+}
+
+/**
+ * Get only one on record by id 
+ */
+function selectOnestudent($id) {
+
+}
+
+/**
+ * Delete student by id
+ */
+function deleteStudent($id) {
+
+}
+
+
+/**
+ * Update students
+ * 
+ */
+function updateStudent($firstName, $LastName) {
+
+}
---
+++ crud_homework/index.php
@@ -0,0 +1,25 @@
+<?php require_once('partial/header.php'); ?>
+    <div class="container p-4">
+        <div class="d-flex justify-content-end p-2">
+            <a href="" class="btn btn-primary">Add +</a>
+        </div>
+        <div class="card">
+            <div class="card-body">
+               <div class="d-flex">
+                    <div class="card-image mr-3">
+                        <img class="img-fluid" width="200" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Young_girl_smiling_in_sunshine_%282%29.jpg/1200px-Young_girl_smiling_in_sunshine_%282%29.jpg" alt="">
+                    </div>
+                    <div class="info">
+                        <h1 class="display-4">Name: Kanha </h1>
+                        <strong>Age: 10</strong> 
+                        <p>Email: kanha@gmail.com</p>
+                    </div>
+               </div>
+                <div class="action d-flex justify-content-end">
+                    <a href="" class="btn btn-primary btn-sm mr-2"><i class="fa fa-pencil"></i></a>
+                    <a href="" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php require_once('partial/footer.php'); ?>
